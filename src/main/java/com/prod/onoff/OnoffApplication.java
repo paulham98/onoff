f03fb61af910482ba323aaf6b853d6c1224bfea4
paulham <pauljungho.ham@gmail.com>
2023-04-05 15:53:40 +0900

    test error

--- src/main/java/com/prod/onoff/OnoffApplication.java
+++ src/main/java/com/prod/onoff/OnoffApplication.java
@@ -1,11 +1,13 @@
 package com.prod.onoff;
 
+import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@AllArgsConstructor
 public class OnoffApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(OnoffApplication.class, args);
     }
--- src/main/java/com/prod/onoff/controller/HomeController.java
+++ src/main/java/com/prod/onoff/controller/HomeController.java
@@ -1,2 +1,14 @@
-package com.prod.onoff.controller;public class HomeController {
+package com.prod.onoff.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+    @GetMapping(value={"/", ""})
+    public String home(){
+        return "home";
+    }
 }
